a129974deb7d6b1749555edbe058d9cc4680ff8b
dubbril <dubbril@gmail.com>
2017-09-19 00:00:36 +0700

    First Commit

---
+++ src/client/ClientTest.java
@@ -0,0 +1,27 @@
+package client;
+
+import dao.CustomerDAO;
+import annotation.Customer;
+import util.HibernateUtil;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+		Customer cust1 = new Customer("JACK", "Marlet", 15);
+		Customer cust2 = new Customer("YAN", "BILLANA", 20);
+
+		try {
+			HibernateUtil.beginTransaction();
+			CustomerDAO custdao = new CustomerDAO();
+			custdao.insert(cust1);
+			custdao.insert(cust2);
+			HibernateUtil.commitTransaction();
+			System.out.println("DONE");
+
+		} catch (Exception ex) {
+			System.out.println("ERROR");
+			HibernateUtil.rollbackTransaction();
+		}
+	}
+
+}
